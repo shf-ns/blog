b38c2beb78511c52d69eb68f6ef41a4ea2a794a7
shf-ns <3104467468@qq.com>
2026-04-20 22:04:17 +0800

    设置相关样式第2版

--- src/pages/Setting/index.vue
+++ src/pages/Setting/index.vue
@@ -1,27 +1,32 @@
-<script setup lang="ts" name="Setting"></script>
+<script setup lang="ts" name="Setting">
+import { ref } from 'vue'
+
+
+</script>
 
 <template>
   <div class="box">
     <div class="theme">
-      <div class="title">
-        <span>主题：</span>
-        <img src="/src/assets/箭头 上.png" alt="">
-      </div>
-      <ul class="theme-choose">
-        <li>浅色</li>
-        <li>深色</li>
-        <li>跟随浏览器</li>
-      </ul>
+      <span class="title">主题：</span>
+      <label for="light" class="item">
+        <input type="radio" id="light" style="display: none;">
+        <span>浅色</span>
+      </label>
+      <label for="dark" class="item">
+        <input type="radio" id="dark" style="display: none;">
+        <span>深色</span>
+      </label>
     </div>
     <div class="language">
-      <div class="title">
-        <span>语言：</span>
-        <img src="/src/assets/箭头 上.png" alt="">
-      </div>
-      <ul class="language-choose">
-        <li>简体中文</li>
-        <li>英文</li>
-      </ul>
+      <span class="title">语言：</span>
+      <label for="Chinese" class="item">
+        <input type="radio" id="Chinese" style="display: none;">
+        <span>简体中文</span>
+      </label>
+      <label for="English" class="item">
+        <input type="radio" id="English" style="display: none;">
+        <span>英文</span>
+      </label>
     </div>
   </div>
 </template>
@@ -31,38 +36,38 @@
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  max-width: 700px;
+  max-width: 500px;
   height: 55vh;
   margin: 100px 200px;
-  text-align: center;
+}
+
+.theme,
+.language {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .theme .title,
 .language .title {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 60px;
-  padding: 0 20px;
-  border: 1px solid #bcbdbe;
+  font-size: 25px;
+  font-weight: 600;
 }
 
-.theme .title span,
-.language .title span {
-  font-size: 30px;
-  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.467);
-}
-
-.theme .title img,
-.language .title img {
-  height: 25px;
-}
-
-.theme-choose li,
-.language-choose li {
+.theme .item,
+.language .item {
+  width: 90px;
   height: 40px;
-  border: 1px solid #e5e7e9;
+  border: 1px solid;
+  font-size: 18px;
   text-align: center;
   line-height: 40px;
+  border: 1px solid rgb(166, 166, 166);
+  border-radius: 10px;
+}
+
+.theme .item:hover,
+.language .item:hover {
+  box-shadow: 0 10px 16.4px #d0d3d4;
 }
 </style>
