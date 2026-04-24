ced6c9965ade677c5c4dfeedf50a0ae352f971c8
shf-ns <3104467468@qq.com>
2026-04-24 11:28:18 +0800

    格式化 vite.config.ts 文件，统一引号风格并确保基础 URL 设置为 /blog/

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import vueSetupExtend from 'vite-plugin-vue-setup-extend'
-import VueRouter from "vue-router/vite"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import vueSetupExtend from "vite-plugin-vue-setup-extend";
+import VueRouter from "vue-router/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,4 +19,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: "/blog/",
 });
