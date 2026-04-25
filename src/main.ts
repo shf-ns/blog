10b9edd70bf27ac51ccd3be4f94496e4978f0016
shf-ns <3104467468@qq.com>
2026-04-25 16:10:59 +0800

    更新路由配置，将历史模式从 createWebHashHistory 修改为 createWebHistory

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(
   createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: routes,
   }),
 );
