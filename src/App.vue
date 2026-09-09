06fa14799d75455c96f396be10ab8481b1564020
shf-ns <3104467468@qq.com>
2026-09-09 08:39:24 +0800

    feat(App): 添加侧边栏导航组件
    添加了包含头像和菜单项的侧边栏组件，用于提供导航功能。
    引入了新的CSS样式来支持侧边栏布局和样式设计。
    更新了基础样式以隐藏溢出内容。

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,70 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="sider">
+    <div class="sider-avatar">
+      <img src="./assets/img/avatar.png" alt="头像">
+    </div>
+    <ul class="sider-menu">
+      <li>
+        <div class="icon">
+          <img src="./assets/img/house.png" alt="首页">
+        </div>
+        首页
+      </li>
+      <li>
+        <div class="icon">
+          <img src="./assets/img/project.png" alt="GitHub项目">
+        </div>
+        GitHub项目
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.sider {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  height: 100%;
+  padding: 20px 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.sider-avatar {
+  width: 100px;
+  height: 100px;
+  margin-top: 40px;
+  border-radius: 50%;
+}
+
+.sider-avatar img {
+  width: 100%;
+  height: 100%;
+}
+
+.sider-menu {
+  width: 100%;
+  margin-top: 120px;
+}
+
+.sider-menu li {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 40px;
+  font-size: 18px;
+}
+
+.sider-menu .icon {
+  width: 30px;
+  height: 30px;
+}
+
+.sider-menu .icon img {
+  width: 100%;
+  height: 100%;
+}
+</style>
