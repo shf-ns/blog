c2a70e950ef24549b2c2d7f4f1ddecc619d3ce86
shf-ns <3104467468@qq.com>
2026-09-14 22:52:02 +0800

    feat(projects): 添加页脚组件并优化项目页面布局
    - 引入 Footer 组件到项目页面
    - 调整页面整体布局样式，添加 flex 布局支持
    - 优化项目描述文本的多行显示和省略效果
    - 添加响应式设计适配不同屏幕尺寸
    - 调整项目列表间距和内边距设置

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,4 +1,6 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Footer } from '@/layout'
+</script>
 
 <template>
     <div class="projects">
@@ -47,7 +49,8 @@
             </li>
             <li class="item">
                 <a href="#" class="title">learn-Vue3</a>
-                <p class="description">学习 Vue3 的项目</p>
+                <p class="description">学习 Vue3 的项目学习 Vue3 的项目学习 Vue3 的项目学习 Vue3 的项目学习 Vue3 的项目学习 Vue3 的项目学习 Vue3 的项目学习
+                    Vue3 的项目</p>
                 <div class="info">
                     <span class="lang">Vue3</span>
                     <div class="stars">
@@ -59,22 +62,25 @@
                     <span class="updated">updated：2023-08-01</span>
                 </div>
             </li>
-
         </ul>
+        <Footer />
     </div>
 </template>
 
 <style scoped>
 .projects {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
     width: 100%;
     height: 100%;
+    padding: 0 200px;
 }
 
 .projects-list {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-gap: 30px;
-    padding: 0 200px;
     margin-top: 70px;
 }
 
@@ -103,6 +109,13 @@
 .projects-list .item .description {
     font-size: 14px;
     color: #59636e;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    line-clamp: 2;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
 }
 
 .projects-list .item .info {
@@ -126,5 +139,41 @@
 .projects-list .item .info .stars .img img {
     width: 100%;
     height: 100%;
+}
+
+@media (min-width: 1024px) and (max-width: 1240px) {
+    .projects {
+        padding: 0 80px;
+    }
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+    .projects {
+        padding: 0 90px;
+    }
+
+    .projects-list {
+        grid-template-columns: repeat(1, 1fr);
+        margin-top: 40px;
+    }
+
+    .footer {
+        margin-top: 25px;
+    }
+}
+
+@media (max-width: 768px) {
+    .projects {
+        padding: 0 60px;
+    }
+
+    .projects-list {
+        grid-template-columns: repeat(1, 1fr);
+        margin-top: 120px;
+    }
+
+    .footer {
+        margin-top: 25px;
+    }
 }
 </style>
